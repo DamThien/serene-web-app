be219efcc6dda3dca3a1fac7c8991e202ca21e0a
Thien Dam <thiendam052@gmail.com>
2026-04-08 14:28:02 +0700

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/serene-web-app/',
   build: { outDir: 'dist' },
   server: {
     proxy: {
